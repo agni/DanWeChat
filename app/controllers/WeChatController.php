4159c973db0fa2b25e23b41e848b8c08521d07e7
chenyingzhou <echocyz@gmail.com>
2018-08-16 16:31:42 +0800

    登录逻辑转移到model

--- app/controllers/WeChatController.php
+++ app/controllers/WeChatController.php
@@ -2,8 +2,6 @@
 
 namespace Dandelion\Controllers;
 
-use Dandelion\Http;
-use Dandelion\Models\MiniProgram;
 use Dandelion\Models\WeChatUser;
 
 class WeChatController extends ControllerBase
@@ -19,27 +17,10 @@ class WeChatController extends ControllerBase
     {
         $appKey = $this->request->getHeader("appKey");
         $code = $this->getJson(["code"])->code;
-        $app = MiniProgram::findFirst(["appKey = \"$appKey\""]);
-        $weChatRes = Http::get("https://api.weixin.qq.com/sns/jscode2session", [
-            "appid"      => $app->appId,
-            "secret"     => $app->appSecret,
-            "js_code"    => $code,
-            "grant_type" => "authorization_code",
-        ]);
-        $weChatRes = json_decode($weChatRes);
-        $openId = $weChatRes->openid;
-        $unionId = $weChatRes->unionid ?? "";
-        $appUser = WeChatUser::findFirst(["openId = \"$openId\""]);
-        if (!$appUser) {
-            $appUser = new WeChatUser();
-            $appUser->openId = $openId;
-            $appUser->unionId = $unionId;
-            $appUser->appId = $app->appId;
-            $appUser->save();
+        if (!WeChatUser::login($appKey, $code)) {
+            return $this->sendFail("登录失败");
         }
-        $this->session->set("openId", $openId);
-        $this->session->set("appId", $app->id);
-        return $this->sendMessage("OK");
+        return $this->sendMessage("登录成功");
     }
 
 }
--- app/models/WeChatUser.php
+++ app/models/WeChatUser.php
@@ -7,6 +7,9 @@
 namespace Dandelion\Models;
 
 
+use Dandelion\Http;
+use Phalcon\Di;
+
 class WeChatUser extends ModelBase
 {
     public $id;
@@ -36,8 +39,44 @@ class WeChatUser extends ModelBase
         return "wechat_user";
     }
 
-    public function login()
+    /**
+     * 小程序登录，验证小程序是否存在，登录成功将重要信息写入session
+     *
+     * @param $appKey
+     * @param $code
+     * @return bool
+     */
+    public static function login($appKey, $code)
     {
+        $app = MiniProgram::findFirst(["appKey = \"$appKey\""]);
+        if (!$app) {
+            return false;
+        }
+        $weChatRes = Http::get("https://api.weixin.qq.com/sns/jscode2session", [
+            "appid"      => $app->appId,
+            "secret"     => $app->appSecret,
+            "js_code"    => $code,
+            "grant_type" => "authorization_code",
+        ]);
+        $weChatRes = json_decode($weChatRes);
+        if (!isset($weChatRes->openid)) {
+            return false;
+        }
 
+        $openId = $weChatRes->openid;
+        $unionId = $weChatRes->unionid ?? "";
+        $appUser = static::findFirst(["openId = \"$openId\""]);
+        if (!$appUser) {
+            $appUser = new static();
+            $appUser->assign([
+                "openId"  => $openId,
+                "unionId" => $unionId,
+                "appId"   => $app->appId,
+            ]);
+            $appUser->save();
+        }
+        Di::getDefault()->get("session")->set("openId", $openId);
+        Di::getDefault()->get("session")->set("appId", $app->id);
+        return true;
     }
 }
