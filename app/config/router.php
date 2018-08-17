40cd26e1111cb5bbe3931581515c32b06a328007
chenyingzhou <echocyz@gmail.com>
2018-08-17 15:24:31 +0800

    优化代码风格

--- app/config/router.php
+++ app/config/router.php
@@ -7,15 +7,13 @@ $router->setDefaultNamespace("Dandelion\\Controllers");
 
 $routersDir = APP_PATH . "/config/router/";
 $files = new \FilesystemIterator($routersDir);
-$filesBasename = [];
+
 /** @var \FilesystemIterator $file */
 foreach ($files as $file) {
     if (!$file->isFile() || "php" !== $file->getExtension()) {
         continue;
     }
-    $filesBasename[] = $file->getBasename(".php");
-}
-foreach ($filesBasename as $className) {
+    $className = $file->getBasename(".php");
     include $routersDir . $className . ".php";
     $router->mount(new $className);
 }
